9b12c8c3642543adc38dba7ebc19a2a3d596bdbf
Aboulaye Drabo <drabis47@gmail.com>
2021-10-11 15:49:43 -0400

    header

--- parentsocial/src/components/Header/Header.js
+++ parentsocial/src/components/Header/Header.js
@@ -23,36 +23,22 @@ import {
     return (
       <div>
         <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
+          <NavbarBrand href="/">PARENTS SOCIAL</NavbarBrand>
             <Nav className="mr-auto" navbar>
               <NavItem>
-                <NavLink href="/components/">Components</NavLink>
+                <NavLink href="/playDay/">Play-day</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                <NavLink href="/playDay/">Daycare</NavLink>
               </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Interview Clothing</NavLink>
+              </NavItem>
             </Nav>
-            <NavbarText>Simple Text</NavbarText>
-          </Collapse>
+            <NavbarText>Interview-Prep</NavbarText>
+            <NavItem>
+                <NavLink href="/login">Logout</NavLink>
+              </NavItem>
         </Navbar>
       </div>
     );
--- parentsocial/src/components/PostCard/PostCard.js
+++ parentsocial/src/components/PostCard/PostCard.js
@@ -9,7 +9,7 @@ import {
 
   const PostCard = (props) => {
     return (
-        <>
+     <>
         <Row>
         <Col sm="6">
       <CardGroup >
@@ -38,32 +38,32 @@ import {
 <br/>
 
 <Row>
-<Col sm="6">
-<CardGroup >
-<Card>
-  <CardImg top width="100%" src={Image} alt="Card image cap" />
-  <CardBody>
-    <CardTitle tag="h5">Card title</CardTitle>
-    <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-    <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-    <Button>Button</Button>
-  </CardBody>
-</Card>
-<br/>
-<Card>
-  <CardImg top width="100%" margin-left="10%" src={Image} alt="Card image cap" />
-  <CardBody>
-    <CardTitle tag="h5">Card title</CardTitle>
-    <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-    <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-    <Button>Button</Button>
-  </CardBody>
-</Card>
-</CardGroup>
-</Col>
+    <Col sm="6">
+    <CardGroup >
+    <Card>
+    <CardImg top width="100%" src={Image} alt="Card image cap" />
+    <CardBody>
+        <CardTitle tag="h5">Card title</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+        <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+        <Button>Button</Button>
+    </CardBody>
+    </Card>
+    <br/>
+    <Card>
+    <CardImg top width="100%" margin-left="10%" src={Image} alt="Card image cap" />
+    <CardBody>
+        <CardTitle tag="h5">Card title</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+        <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+        <Button>Button</Button>
+    </CardBody>
+    </Card>
+    </CardGroup>
+    </Col>
 </Row>
   
-      </> 
+    </> 
     );
   };
 
